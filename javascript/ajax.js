182ebb76b13f1acd9686ece842b963106b21d758
Luis-Salinas-SD <pasu.impala21@gmail.com>
2021-01-02 15:30:19 -0600

    Actualizacion AJAX

---
+++ javascript/ajax.js
@@ -0,0 +1,18 @@
+//! Consumir API con AJAX
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.send();
+
+function respuesta() {
+    console.log(this.responseText);
+}
+xhr.addEventListener("load", respuesta);
+
+
+//! Ejemplo de addEventListener()
+
+function res() {
+    document.getElementById("demo").innerHTML = "Hola a Todos Morefokers hoy es navidarks!";
+}
+document.addEventListener("click", res);
